b6227650a94962e6837392a2b73fe1e1821e5c2d
Mr.ordinary 824 <amannelson824@gmail.com>
2026-06-27 18:48:54 +0300

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,32 @@
+document.addEventListener("scroll",()=>{
 
+document.querySelectorAll(".card")
+.forEach(card=>{
+
+const r=
+card.getBoundingClientRect();
+
+if(r.top<window.innerHeight-80){
+
+card.style.opacity=1;
+
+card.style.transform=
+"translateY(0)";
+}
+
+});
+
+});
+
+document.querySelectorAll(".card")
+.forEach(el=>{
+
+el.style.opacity=0;
+
+el.style.transform=
+"translateY(40px)";
+
+el.style.transition=
+".8s";
+
+});
