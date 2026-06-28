89e2b2815bb5cfab65687300d56dab44f3e2b0a6
Mr.ordinary 824 <amannelson824@gmail.com>
2026-06-28 14:50:34 +0300

    Update server.js

--- assets/aman-safari-backend/server.js
+++ assets/aman-safari-backend/server.js
@@ -1,1 +1,46 @@
+import express from "express";
+import cors from "cors";
 
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+let bookings=[];
+
+
+// receive booking
+
+app.post("/book",(req,res)=>{
+
+const booking=req.body;
+
+bookings.push(booking);
+
+console.log("New booking:",booking);
+
+res.json({
+
+success:true,
+
+message:"Safari booked"
+
+});
+
+});
+
+
+// view bookings
+
+app.get("/bookings",(req,res)=>{
+
+res.json(bookings);
+
+});
+
+app.listen(3000,()=>{
+
+console.log("Running");
+
+});
